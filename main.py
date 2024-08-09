d690585c7966f7da257149afcdced5edb35afc49
hezychacha <hezychacha@gmail.com>
2024-08-10 02:26:20 +0300

    include the status CODE to the API

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -52,7 +52,7 @@ def get_posts():
 #     print(post.dict())
 #     return {"data": post}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post:Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 10000000)
@@ -71,9 +71,19 @@ def get_latest_post():
     post = my_posts[len(my_posts)-1]
     return{"detail": post}
 
+# @app.get("/posts/{id}")
+# def get_posts(id: int):
+#     post = find_post(id)
+#     print(post)
+#     return{"post_detail": post}
+
 @app.get("/posts/{id}")
-def get_posts(id: int):
+def get_posts(id: int, response: Response):
     post = find_post(id)
-    print(post)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return{'message': f"post with id: {id} was not found"}
     return{"post_detail": post}
 
